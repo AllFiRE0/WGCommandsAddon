e51dd22c66870e825a64929123fd88baefdb6e08
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-07-24 00:12:34 +0300

    Create WGCommandsAddon.java

---
+++ main/java/ru/allfire/wgcommandsaddon/WGCommandsAddon.java
@@ -0,0 +1,153 @@
+package ru.allfire.wgcommandsaddon;
+
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.StringFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import ru.allfire.wgcommandsaddon.events.RegionEnterEvent;
+import ru.allfire.wgcommandsaddon.events.RegionLeaveEvent;
+import ru.allfire.wgcommandsaddon.handlers.RegionSessionHandler;
+
+public final class WGCommandsAddon extends JavaPlugin implements Listener {
+
+    // Флаги
+    public static StringFlag ONE_COMMAND_ASCONSOLE_FLAG;
+    public static StringFlag ONE_COMMAND_ASPLAYER_FLAG;
+    public static StringFlag ONE_PERM_COMMAND_ASCONSOLE_FLAG;
+    public static StringFlag ONE_PERM_COMMAND_ASPLAYER_FLAG;
+    
+    private static WGCommandsAddon instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+        FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
+        
+        try {
+            // Регистрируем флаги
+            ONE_COMMAND_ASCONSOLE_FLAG = new StringFlag("one-command-asconsole");
+            registry.register(ONE_COMMAND_ASCONSOLE_FLAG);
+
+            ONE_COMMAND_ASPLAYER_FLAG = new StringFlag("one-command-asplayer");
+            registry.register(ONE_COMMAND_ASPLAYER_FLAG);
+
+            ONE_PERM_COMMAND_ASCONSOLE_FLAG = new StringFlag("one-perm-command-asconsole");
+            registry.register(ONE_PERM_COMMAND_ASCONSOLE_FLAG);
+
+            ONE_PERM_COMMAND_ASPLAYER_FLAG = new StringFlag("one-perm-command-asplayer");
+            registry.register(ONE_PERM_COMMAND_ASPLAYER_FLAG);
+            
+            getLogger().info("Все флаги успешно зарегистрированы!");
+            getLogger().info("  - one-command-asconsole");
+            getLogger().info("  - one-command-asplayer");
+            getLogger().info("  - one-perm-command-asconsole");
+            getLogger().info("  - one-perm-command-asplayer");
+        } catch (FlagConflictException e) {
+            getLogger().warning("Ошибка регистрации флагов!");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        
+        try {
+            com.sk89q.worldguard.session.SessionManager sessionManager = 
+                WorldGuard.getInstance().getPlatform().getSessionManager();
+            sessionManager.registerHandler(RegionSessionHandler.FACTORY, null);
+            getLogger().info("Обработчик сессий WorldGuard зарегистрирован!");
+        } catch (Exception e) {
+            getLogger().warning("Ошибка регистрации обработчика сессий!");
+            e.printStackTrace();
+        }
+        
+        getLogger().info("========================================");
+        getLogger().info("ДополнительныеКомандыWG v" + getDescription().getVersion());
+        getLogger().info("Автор: AllF1RE");
+        getLogger().info("Флаги:");
+        getLogger().info("  - one-command-asconsole (консоль, без прав)");
+        getLogger().info("  - one-command-asplayer (игрок, без прав)");
+        getLogger().info("  - one-perm-command-asconsole (консоль, с правом wgca.onecommand)");
+        getLogger().info("  - one-perm-command-asplayer (игрок, с правом wgca.onecommand)");
+        getLogger().info("========================================");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ДополнительныеКомандыWG выключен.");
+    }
+
+    public static WGCommandsAddon getInstance() {
+        return instance;
+    }
+
+    @EventHandler
+    public void onRegionEnter(RegionEnterEvent event) {
+        handleCommands(event.getPlayer(), event.getRegion(), true);
+    }
+
+    @EventHandler
+    public void onRegionLeave(RegionLeaveEvent event) {
+        handleCommands(event.getPlayer(), event.getRegion(), false);
+    }
+
+    private void handleCommands(Player player, ProtectedRegion region, boolean isEnter) {
+        String action = isEnter ? "вход" : "выход";
+        String regionName = region.getId();
+
+        // 1. one-command-asconsole (консоль, без прав)
+        String consoleCommand = region.getFlag(ONE_COMMAND_ASCONSOLE_FLAG);
+        if (consoleCommand != null && !consoleCommand.isEmpty()) {
+            executeCommand(player, consoleCommand, false);
+            getLogger().info("[one-command-asconsole] " + player.getName() + " при " + action + " в " + regionName);
+        }
+
+        // 2. one-command-asplayer (игрок, без прав)
+        String playerCommand = region.getFlag(ONE_COMMAND_ASPLAYER_FLAG);
+        if (playerCommand != null && !playerCommand.isEmpty()) {
+            executeCommandAsPlayer(player, playerCommand);
+            getLogger().info("[one-command-asplayer] " + player.getName() + " при " + action + " в " + regionName);
+        }
+
+        // 3. one-perm-command-asconsole (консоль, с правом)
+        String permConsoleCommand = region.getFlag(ONE_PERM_COMMAND_ASCONSOLE_FLAG);
+        if (permConsoleCommand != null && !permConsoleCommand.isEmpty()) {
+            if (player.hasPermission("wgca.onecommand")) {
+                executeCommand(player, permConsoleCommand, true);
+                getLogger().info("[one-perm-command-asconsole] " + player.getName() + " при " + action + " в " + regionName);
+            } else {
+                getLogger().info("[one-perm-command-asconsole] " + player.getName() + " не имеет права wgca.onecommand");
+            }
+        }
+
+        // 4. one-perm-command-asplayer (игрок, с правом)
+        String permPlayerCommand = region.getFlag(ONE_PERM_COMMAND_ASPLAYER_FLAG);
+        if (permPlayerCommand != null && !permPlayerCommand.isEmpty()) {
+            if (player.hasPermission("wgca.onecommand")) {
+                executeCommandAsPlayer(player, permPlayerCommand);
+                getLogger().info("[one-perm-command-asplayer] " + player.getName() + " при " + action + " в " + regionName);
+            } else {
+                getLogger().info("[one-perm-command-asplayer] " + player.getName() + " не имеет права wgca.onecommand");
+            }
+        }
+    }
+
+    // Выполнение команды от консоли
+    private void executeCommand(Player player, String command, boolean checkPermission) {
+        String parsedCommand = command.replace("{player}", player.getName());
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), parsedCommand);
+    }
+
+    // Выполнение команды от игрока
+    private void executeCommandAsPlayer(Player player, String command) {
+        String parsedCommand = command.replace("{player}", player.getName());
+        player.performCommand(parsedCommand);
+    }
+}
