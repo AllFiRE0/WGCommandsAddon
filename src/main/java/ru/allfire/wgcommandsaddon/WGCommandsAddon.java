0e8b2400886aaa54bd889b5b88e470283fd5812c
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-07-24 11:46:01 +0300

    Update WGCommandsAddon.java

--- src/main/java/ru/allfire/wgcommandsaddon/WGCommandsAddon.java
+++ src/main/java/ru/allfire/wgcommandsaddon/WGCommandsAddon.java
@@ -17,6 +17,7 @@ public final class WGCommandsAddon extends JavaPlugin {
     
     private static WGCommandsAddon instance;
     private boolean placeholderAPIEnabled = false;
+    private boolean disableLogs = false;
 
     @Override
     public void onLoad() {
@@ -37,10 +38,6 @@ public final class WGCommandsAddon extends JavaPlugin {
             registry.register(MORE_PERM_CMD_CONSOLE_FLAG);
             
             getLogger().info("Все флаги успешно зарегистрированы!");
-            getLogger().info("  - more-cmd-player");
-            getLogger().info("  - more-cmd-console");
-            getLogger().info("  - more-perm-cmd-player");
-            getLogger().info("  - more-perm-cmd-console");
         } catch (FlagConflictException e) {
             getLogger().warning("Ошибка регистрации флагов!");
             e.printStackTrace();
@@ -49,31 +46,39 @@ public final class WGCommandsAddon extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Загружаем конфиг
+        saveDefaultConfig();
+        reloadConfig();
+        disableLogs = getConfig().getBoolean("disable-logs", false);
+        
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             placeholderAPIEnabled = true;
-            getLogger().info("PlaceholderAPI найден! Внешние заполнители поддерживаются.");
+            if (!disableLogs) getLogger().info("PlaceholderAPI найден!");
         } else {
-            getLogger().warning("PlaceholderAPI НЕ найден! Внешние заполнители не будут работать.");
+            if (!disableLogs) getLogger().warning("PlaceholderAPI НЕ найден!");
         }
         
-        getServer().getPluginManager().registerEvents(new PlayerListener(this, placeholderAPIEnabled), this);
+        getServer().getPluginManager().registerEvents(new PlayerListener(this, placeholderAPIEnabled, disableLogs), this);
         
-        getLogger().info("========================================");
-        getLogger().info("WGCommandsAddon v" + getDescription().getVersion());
-        getLogger().info("Автор: AllF1RE");
-        getLogger().info("Флаги:");
-        getLogger().info("  - more-cmd-player (игрок, все)");
-        getLogger().info("  - more-cmd-console (консоль, все)");
-        getLogger().info("  - more-perm-cmd-player (игрок, только обычные игроки)");
-        getLogger().info("  - more-perm-cmd-console (консоль, только обычные игроки)");
-        getLogger().info("Формат: 'задержка_сек команда||задержка_сек команда'");
-        getLogger().info("Пример: '10 give {player} bone 1||30 give {player} diamond 1'");
-        getLogger().info("========================================");
+        if (!disableLogs) {
+            getLogger().info("========================================");
+            getLogger().info("WGCommandsAddon v" + getDescription().getVersion());
+            getLogger().info("Автор: AllF1RE");
+            getLogger().info("Флаги:");
+            getLogger().info("  - more-cmd-player (игрок, все)");
+            getLogger().info("  - more-cmd-console (консоль, все)");
+            getLogger().info("  - more-perm-cmd-player (игрок, право wgca.use)");
+            getLogger().info("  - more-perm-cmd-console (консоль, право wgca.use)");
+            getLogger().info("Формат: 'задержка_сек команда||задержка_сек команда'");
+            getLogger().info("Пример: '10 give {player} bone 1||30 give {player} diamond 1'");
+            getLogger().info("Логи в консоль: " + (disableLogs ? "ВЫКЛЮЧЕНЫ" : "ВКЛЮЧЕНЫ"));
+            getLogger().info("========================================");
+        }
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("WGCommandsAddon выключен.");
+        if (!disableLogs) getLogger().info("WGCommandsAddon выключен.");
     }
 
     public static WGCommandsAddon getInstance() {
@@ -82,5 +87,9 @@ public final class WGCommandsAddon extends JavaPlugin {
     
     public boolean isPlaceholderAPIEnabled() {
         return placeholderAPIEnabled;
+    }
+    
+    public boolean isLogsDisabled() {
+        return disableLogs;
     }
 }
